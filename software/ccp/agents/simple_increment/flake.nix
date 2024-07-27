c24ffa67f97ebfdbd6d96970bb2eed6af0f18096
Steven Kane <Steven Kane>
2024-07-27 12:36:00 -0700

    Moved simple_increment from engines to agents

---
+++ software/ccp/agents/simple_increment/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A Rust project that compiles to WebAssembly (WASM) with WASI support using fenix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs = { self, nixpkgs, fenix }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      rust = fenix.packages.x86_64-linux.stable.toolchain;
+    in
+    {
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            rust
+            pkgs.wasmtime
+            pkgs.wasilibc
+            pkgs.git        # Additional package
+            pkgs.htop       # Additional package
+          ];
+
+          shellHook = ''
+            echo "Development environment for Rust WASM project with WASI support using fenix"
+            export RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals'
+          '';
+        };
+      };
+
+      packages = {
+        default = pkgs.stdenv.mkDerivation {
+          name = "your-package-name";
+          src = ./.;
+          # Add your build instructions here
+        };
+      };
+    };
+}
+
